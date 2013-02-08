ebc7cbf52965f09021c6be35042bd3c17b106137
Globegitter <markus.padourek@gmail.com>
2013-02-08 13:45:30 +0000

    Created db Model using EF

---
+++ OccupOSModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\Markus\Documents\Visual Studio 2012\Projects\C#\UCL Year 2\OccupOS\OccupOSMonitor\OccupOSMonitor\OccupOSModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
