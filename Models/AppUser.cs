836f826fbc0877aebf546569aefd890cd595ba47
Globegitter <markus.padourek@gmail.com>
2013-03-02 19:29:18 +0000

    Updated Copyright notice.

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,7 +1,14 @@
-using System;
-using System.Text;
-using System.Collections.Generic;
-
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AppUser.cs" company="OccupOS">
+//   This file is part of OccupOS.
+//   OccupOS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//   OccupOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//   You should have received a copy of the GNU General Public License along with OccupOS.  If not, see <http://www.gnu.org/licenses/>.
+// </copyright>
+// <summary>
+//   Defines the AppUser type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace OccupOSMonitor.Models {
 
