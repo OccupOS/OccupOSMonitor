91e91bc939d1eb7303bf91db1dcc52ce04aa49e6
Globegitter <markus.padourek@gmail.com>
2013-03-11 11:17:20 +0000

    Added package restre to nuget.cofig

--- Scripts/App/Routes/MonitorRoute.js
+++ Scripts/App/Routes/MonitorRoute.js
@@ -1,5 +1,30 @@
-﻿OccupOS.MonitorRoute = Ember.Route.extend({
+﻿/**
+    <Description of Route>
+
+    @class
+    @namespace OccupOS
+    @extends Ember.Route
+
+    further examples of use:
+    @param {string} title - The title of the book.
+    @param {string} author - The author of the book.
+ */
+
+OccupOS.MonitorRoute = Ember.Route.extend({
+    /**
+        @private (Note: ??really?? Not sure, just copied example from ember.js code)
+
+        Connects the Controller with the Models. Loads SensorData from server using the API.
+
+        @method setupController
+    */
     setupController: function (controller, model) {
+        // Note: The following comments are just for VS2012 intellisense. Consider using it (or not).
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="model"></param>
         controller.set('content', OccupOS.Sensordata.find());
     }
 });
