28cd1ea7b5c5da02785b7d39e3bb97263693828f
zcabh25@live.ucl.ac.uk <zcabh25@live.ucl.ac.uk>
2013-03-25 10:57:07 +0000

    Updated json

--- Scripts/App/Views/IndexView.js
+++ Scripts/App/Views/IndexView.js
@@ -9,9 +9,11 @@ OccupOS.IndexView = Ember.View.extend({
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-        var parseDate = d3.time.format("%d-%b-%y").parse;
-
-        var x = d3.time.scale()
+        var parseDate = d3.time.format.iso.parse;
+      //   var parseDate = d3.time.format("%Y-%m-%dT%H:%M:%S.%L").parse;
+        //var parseDate = d3.time.format("%d-%b-%y").parse;
+          var x = d3.time.scale.utc()
+          //  var x = d3.scale.linear()
             .range([0, width]);
 
         var y = d3.scale.linear()
@@ -47,24 +49,33 @@ OccupOS.IndexView = Ember.View.extend({
           .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var myjson = '{"sensors": [ { "MeasuredData": "434", "MeasuredAt": "/Date(1363876464000+0000)/"}, { "MeasuredData": "654", "MeasuredAt": "/Date(1363876464000+0000)/" } ]}';
-        json = JSON.parse(myjson); //add this line
+        
+      
 
-        /*d3.tsv("/Scripts/data.tsv", function (error, data) {
-            data.forEach(function (d) {
-                d.date = parseDate(d.date);
-                d.close = +d.close;
-            });*/
+            $.getJSON("http://localhost:9226/api/v1/sensors?callback=?", function (sensors) {
+            console.log(sensors);
+            sensors.forEach(function (d) {
+
+                d.measuredAt = parseDate(d.measuredAt);
+                d.measuredData = d.measuredData;
+                console.log(d);
+            }); x.domain(d3.extent(sensors, function (d) { return d.measuredAt; }));
+            y.domain(d3.extent(sensors, function (d) { return d.measuredData; }));
+        });
+
+    /*    var myjson = '{"sensors": [ { "MeasuredData": "434", "MeasuredAt": "2013-03-21T14:34:24.0000000"}, { "MeasuredData": "654", "MeasuredAt": "2013-03-22T14:34:24.0000000" } ]}';
+        //  var myjson = '{"sensors": [ { "MeasuredData": "434", "MeasuredAt": "12-Feb-13"}, { "MeasuredData": "654", "MeasuredAt": "14-Feb-13" } ]}';
+        json = JSON.parse(myjson); //add this line
         console.log(json["sensors"]);
         console.log(json["sensors"].length);
         json["sensors"].forEach(function (d) {
             console.log("test");
             d.MeasuredAt = parseDate(d.MeasuredAt);
-            d.MeasuredData = +d.MeasuredData;
-            console.log(d);
+            d.MeasuredData = d.MeasuredData;
+            console.log(d.MeasuredAt);
         });
-            x.domain(d3.extent(json["sensors"], function (d) { return d.MeasuredAt }));
-            y.domain(d3.extent(json["sensors"], function (d) { return d.MeasuredData; }));
+        x.domain(d3.extent(json["sensors"], function (d) { return d.MeasuredAt; }));
+            y.domain(d3.extent(json["sensors"], function (d) { return d.MeasuredData; }));*/
 
             svg.append("g")
                 .attr("class", "x axis")
