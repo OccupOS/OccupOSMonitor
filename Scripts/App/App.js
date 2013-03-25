9810040d1914552e36c6c9f5074cabb83c6c5697
Globegitter <markus.padourek@gmail.com>
2013-03-25 11:44:28 +0000

    Manual merge and added ember debug statements

--- Scripts/App/App.js
+++ Scripts/App/App.js
@@ -1,11 +1,21 @@
 ﻿/// <reference path="../_references.js" />
 
+Ember.LOG_BINDINGS = true;
+
 var OccupOS = Ember.Application.create({
     VERSION: '0.1.0',
     //possibly change to OccupOS Monitor
     name: 'OccupOS',
     author: 'Markus Padourek',
+    LOG_TRANSITIONS: true
     //ready: function() {
     //
     //}
 });
+
+/*Ember.onerror = function (error) {
+    Em.$.ajax('/error-notification', 'POST', {
+        stack: error.stack,
+        otherInformation: 'exception message'
+    });
+};*/
